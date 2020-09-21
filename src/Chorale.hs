a21285ad19d16f553ed4ef0523bd15e241f5d45a
James Carlson <jxxcarlson@gmail.com>
2020-09-21 14:03:18 -0400

    Clean up comments

--- src/Chorale.hs
+++ src/Chorale.hs
@@ -1,13 +1,17 @@
--- An infinite minimalistic chorale in four parts written in Haskell
+-- This compostion is an infinite minimalist chorale in four parts written in Haskell
 -- using the Euterpea library.  We also compute the cycle length
--- for the music.
+-- in beats for each line the music.  With this information in hand,
+-- we can compute the cycle length for the infinite composition: the
+-- smallest number of beats after which the composition repeats itself.
+-- The cycle length is 156780 beats. With this information in hand, we 
+-- can construct a one-cycle version of the chorale, export it to Logic Pro,  
+-- use better sound files, and export the result to mp3.  In fact, the chorale 
+-- is so long that !!we only export the first 200 measures:
 
--- The cycle length of the chorale is 3015 measures, as computed below.  This
--- could easily be made much longer by adding rests so as to make the
--- numbers in the list of individual cycle lengths for
--- bass, tenor, tenor2 and solo (36, 15,12, 67) relatively prime.  For
--- example, the least common multiple of 37, 15, 13, 67 is 483405,
--- so the cycle length is 483405 beats, or 120851.25 4/4 measures.
+-- <audio controls>
+--   <source src="/audio/chorale-2.mp3" type="audio/mpeg">
+--   Your browser does not support the audio element.
+-- </audio>
 
 
 module Chorale where
@@ -16,13 +20,23 @@ import Euterpea
 
 
 -- The composition:
+
+-- Infinite version: 
 chorale :: InstrumentName -> Music Pitch
 chorale player =  instrument player $ bass :=: tenor :=: tenor2 :=: solo
 
--- Example:
--- > playDev 2 $ chorale Bassoon
 
---- Its construction ---
+-- Finite version of the chorale with instrument = clarinet
+chorale' :: Music Pitch
+chorale' = bassLine' :=: tenorLine' :=: tenorLine2' :=: soloLine'
+
+
+-- Examples:
+-- > playDev 2 $ chorale Bassoon
+-- > playDev 2 chorale'
+
+
+--- Construction of the chorale ---
 
 solo :: Music Pitch
 solo = forever $ soloLine
@@ -36,22 +50,31 @@ tenor2 = forever $ tenorLine2
 bass :: Music Pitch
 bass = forever $ bassLine
 
+-- Compute the length of each line
 lSolo = ilength soloLine
 lTenor = ilength tenorLine
 lTenor2 = ilength tenorLine2
 lBass = ilength bassLine
 
+-- Compute the cycle length as the
+-- least common multiple of the lengths
+-- of the individual lines.
 cycleLength = lcm_ [lSolo, lTenor, lTenor2, lBass]
+
+-- Compute the number of times each line of
+-- music must be repeated so as to have
+-- the given cycle length.
 nSolo = cycleLength `div` lSolo
 nTenor = cycleLength `div` lTenor
 nTenor2 = cycleLength `div` lTenor2
 nBass = cycleLength `div` lBass
 
--- Repeat music m n times
+-- Repeat the music m n times
 mrepeat :: Int -> Music a -> Music a
 mrepeat 0 m = rest 0
 mrepeat n m = m :+: mrepeat (n - 1) m
 
+-- Construct one cycle of the chorale
 soloLine', tenorLine', tenorLine2', bassLine' :: Music Pitch
 soloLine' = mrepeat nSolo soloLine 
 tenorLine' = mrepeat nTenor tenorLine
@@ -62,8 +85,8 @@ bassLine' = mrepeat nBass bassLine
 -- > map ilength [bassLine', tenorLine', tenorLine2', soloLine']
 --   [156780,156780,156780,156780]
 
-chorale' = bassLine' :=: tenorLine' :=: tenorLine2' :=: soloLine'
 
+-- The bass line
 bassNote = d 1 
 bassNote' = ds 1
 bassNote'' = e 1
@@ -71,39 +94,24 @@ bassNote''' = f 1
 bassLine = line $ [bassNote 6, rest 3, bassNote' 3, rest 3, bassNote'' 3 
                     , rest 2, bassNote''' 4, rest 1 , bassNote 7, rest 4]
 
+-- The tenor
 tenorNote = a 1
 tenorNote' = c 2
 tenorNote'' = d 2
 tenorLine = line $ map (rescale (1/2)) [rest 2, tenorNote 8, rest 3
                   , tenorNote' 4, tenorNote'' 5, rest 4]
 
+-- The second tenor
 tenorLine2 = rest 7 :+: f 1 4 :+: c 2 4
 
-bigRest :: Music Pitch
-bigRest = rest 9
-
+-- The solo
 soloMotifA = line $ [a 2 1, d 3 1, c 4 2, f 4 hn, rest 2]
 soloMotif = line $ [a 2 1, d 3 1, c 3 2, f 3 2, e 3 4, rest 2]
 soloMotif' = rest 9 :+: soloMotif :+: transpose 3 soloMotif
 soloLine = soloMotif' :+: transpose 7 soloMotif' :+: rest 1
 
 
-
--- COMPUTATION OF THE CYCLE LENGTH
---
--- > mlength soloLine
--- 67 % 1
--- > mlength tenorLine
--- 13 % 1
--- > mlength tenorLine2
--- 15 % 1
--- > mlength bassLine
--- 36 % 1
--- > lcm_ [67, 12, 15, 36]
--- 12060
--- > 12060 `div` 4
--- 3015
-
+-- Compute the length of a melody in beats
 mlength :: Music Pitch -> Dur
 mlength mp =
     case mp of 
@@ -113,9 +121,13 @@ mlength mp =
         m1 :=: m2 -> maximum [mlength m1, mlength m2]
         Modify control m -> mlength m
 
+-- Compute the length of a melody in beats, but convert
+-- to an integer.  Oops, we loose fractional beats,
+-- if there are any.
 ilength :: Music Pitch -> Int 
 ilength mp = round $ mlength mp
 
+-- Least common multiple of a list of integers.
 lcm_ :: [Int] -> Int
 lcm_ [] = 1
 lcm_ (x:[]) = x
